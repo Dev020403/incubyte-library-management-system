a19234a1dc334d83e6242620d8785d8d4aa13ccd
Dev Kapadia <devkapadia123@gmail.com>
2024-08-31 09:40:02 +0530

    Implement Book constructor with parameter validation and add tests

---
+++ src/Book.js
@@ -0,0 +1,17 @@
+
+class Book {
+    constructor(isbn, title, author, year) {
+        //throws error if any of the parameters is missing
+        if (!isbn || !title || !author || !year) {
+            throw new Error('All parameters (isbn, title, author, year) are required');
+        }
+        //initializes the book 
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
+    }
+}
+
+module.exports = Book;
